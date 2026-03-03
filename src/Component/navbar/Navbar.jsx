2e1b8a3e2ec9b3bee034d82451e4f049e89a0d21
Asmothkhan <asmotkhan793@gmail.com>
2026-03-04 00:56:33 +0600

    navbar complete

--- src/Component/navbar/Navbar.jsx
+++ src/Component/navbar/Navbar.jsx
@@ -2,58 +2,6 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-//  <div className="navbar bg-base-100 shadow-sm text-blue-600 px-20 py-10 bg-slate-100">
-  
- 
-//   <div>
-    
-   
-
-//     <a >SwiftCart</a>
-//   </div>
-
-
-//   <div className="navbar-center hidden lg:flex">
-//     <ul className="menu menu-horizontal px-1 text-xl">
-//       <li><a className="nav-btn">Home</a></li>
-//       <li ><a className="nav-btn">Product</a></li>
-//       <li><a className="nav-btn">About</a></li>
-//       <li><a className="nav-btn">Contact</a></li>
-//     </ul>
-//   </div>
-
-  
-//   <div className="navbar-end">
-//     <a className="btn text-2xl text-blue-400 font-bold">
-//       <i className="fa-solid fa-cart-shopping"></i>
-//     </a>
-
-   
-//     <div className="dropdown lg:hidden text-center justify-center px-4">
-//       <div tabindex="0" role="button" className="btn btn-ghost">
-//         <svg xmlns="http://www.w3.org/2000/svg" 
-//           class="h-5 w-5" fill="none" viewBox="0 0 24 24" 
-//           stroke="currentColor">
-//           <path stroke-linecap="round" 
-//           stroke-linejoin="round" 
-//           stroke-width="2" 
-//           d="M4 6h16M4 12h8m-8 6h16" />
-//         </svg>
-//       </div>
-
-//       <div tabindex="0" 
-//           class="menu menu-sm dropdown-content 
-//           bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow  ">
-//         <button id="home-container1"><a className="text-xl py-2 nav-btn ">Home</a></button>
-//     <button  className="nav-btn text-xl py-2">Product</button>
-//         <button><a className="nav-btn text-xl py-2">About</a></button>
-//         <button><a className="nav-btn text-xl py-2">Contact</a></button>
-//       </div>
-//     </div>
-//   </div>
-
-// </div>
-
 <div className="navbar bg-base-100 shadow-sm mx-auto text-center justify-center p-4">
   <div className="navbar-start">
     
